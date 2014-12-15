ac06b41fe81da4a6030cf44c23653993f597b0be
Nicolas 'keksnicoh' Heimann <keksnicoh@heimann.hamburg>
2014-12-15 19:51:15 +0100

    clean up

--- shaders/sphere.f.glsl
+++
@@ -1,8 +0,0 @@
-varying vec2 coord;
-//thanks to https://sites.google.com/site/dlampetest/python/vectorized-particle-system-and-geometry-shaders
-void main() {
-    if (1.0-length(coord)<0.0) {
-    	discard;
-    }
-    gl_FragColor = vec4(1.0, .75, 0.25, -(.7*length( coord )*.7*length( coord ))+.3);
-}
